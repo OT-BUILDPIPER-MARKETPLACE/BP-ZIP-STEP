2a261edf7d5c049c056d6e30a7b1826f8298935c
Aakash Tripathi <aakash.tripathi@opstree.com>
2024-12-04 13:15:05 +0530

    Adding code to zip the cloned repository

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM alpine
 RUN apk add --no-cache --upgrade bash
-RUN apk add jq
+RUN apk add jq zip
 COPY build.sh .
 
 ADD BP-BASE-SHELL-STEPS /opt/buildpiper/shell-functions/
 
 
 ENV SLEEP_DURATION 5s
-ENV ACTIVITY_SUB_TASK_CODE REPLACE_IT
+ENV ACTIVITY_SUB_TASK_CODE ZIP_STEP
 ENV VALIDATION_FAILURE_ACTION WARNING
 
 ENTRYPOINT [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -5,20 +5,13 @@ source /opt/buildpiper/shell-functions/str-functions.sh
 source /opt/buildpiper/shell-functions/file-functions.sh
 source /opt/buildpiper/shell-functions/aws-functions.sh
 
-TASK_STATUS=0
 
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
 sleep  $SLEEP_DURATION
 cd  "${CODEBASE_LOCATION}"
+logInfoMessage "I'll zip the following [$CODEBASE_DIR]"
+zip -r "${CODEBASE_DIR}".zip "${CODEBASE_LOCATION}"
 
-TASK_STATUS=0
-
-if [condition]; then
-    logErrorMessage "Done the required operation"
-else
-    TASK_STATUS=1
-    logErrorMessage "Target server not provided please check"
-
-fi
+TASK_STATUS=$?
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
