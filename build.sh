40223f007f441dfd43b02c68eb9b299a6faae645
Aakash Tripathi <aakash.tripathi@opstree.com>
2024-12-04 19:11:40 +0530

    updating build.sh for timestamp custom tag

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ source /opt/buildpiper/shell-functions/str-functions.sh
 source /opt/buildpiper/shell-functions/file-functions.sh
 source /opt/buildpiper/shell-functions/aws-functions.sh
 
+TAG=$(tail -n 1 data.properties)
+
 
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
@@ -12,7 +14,7 @@ sleep  $SLEEP_DURATION
 cd  "${WORKSPACE}"
 logInfoMessage "I'll zip the following [$CODEBASE_DIR]"
 rm -rf "${CODEBASE_DIR}"/.git
-zip -r "${CODEBASE_DIR}".zip "${CODEBASE_DIR}"
+zip -r "${CODEBASE_DIR}"-"${TAG}".zip "${CODEBASE_DIR}"
 
 TASK_STATUS=$?
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
