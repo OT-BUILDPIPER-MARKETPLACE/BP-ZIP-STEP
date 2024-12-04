d5d79819917d1056139470693a1e8d64b298f993
Aakash Tripathi <aakash.tripathi@opstree.com>
2024-12-04 18:26:57 +0530

    Modifying build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ source /opt/buildpiper/shell-functions/aws-functions.sh
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
 sleep  $SLEEP_DURATION
-cd  "${CODEBASE_LOCATION}"
+cd  "${WORKSPACE}"
 logInfoMessage "I'll zip the following [$CODEBASE_DIR]"
-zip -r "${CODEBASE_DIR}".zip "${CODEBASE_LOCATION}"
+rm -rf "${CODEBASE_DIR}"/.git
+zip -r "${CODEBASE_DIR}".zip "${CODEBASE_DIR}"
 
 TASK_STATUS=$?
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
